61b06dde9af506f78ffa45256b855fcffdeef80f
Domas Lapinskas <domasx2@gmail.com>
2015-06-13 16:58:33 +0300

    update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,18 +3,18 @@
 
 DOCKER_CONFIG=${DOCKER_CONFIG:-docker-compose-prod.yml}
 
-docker-compose -f $DOCKER_CONFIG stop
+./bin/stop_production.sh
 ./bin/db_backup.sh _pre_deploy
 docker-compose build frontend
 docker-compose run --rm frontend gulp build-prod
 
 #cinitialize database container if it's missing
-if  [ $(docker-compose -f $DOCKER_CONFIG ps | grep "dbprod" | wc -l) == 0 ]; then
+if  [ $(docker-compose -f $DOCKER_CONFIG ps | grep "db" | wc -l) == 0 ]; then
     echo "initializing db"
-    docker-compose -f $DOCKER_CONFIG run --rm dbprod postgres --version
+    docker-compose -f $DOCKER_CONFIG run --rm db postgres --version
 fi
 
 docker-compose -f $DOCKER_CONFIG build
 docker-compose -f $DOCKER_CONFIG run --rm djangoprod python manage.py migrate --noinput
 docker-compose -f $DOCKER_CONFIG run --rm djangoprod python manage.py collectstatic --noinput
-docker-compose -f $DOCKER_CONFIG up -d
+./bin/start_production.sh
