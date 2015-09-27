ebb9a3990891fbe08760f1feffda41e760eee978
Domas <domasx2@gmail.com>
2015-09-27 10:27:16 +0300

    fix build_frontend, deprec warnings

--- bin/build_frontend.sh
+++ bin/build_frontend.sh
@@ -2,5 +2,7 @@
 #builds frontend to frontend/dist
 
 DOCKER_CONFIG=${DOCKER_CONFIG:-docker-compose.yml}
+DOCKER_CONFIG_PROD=${DOCKER_CONFIG_PROD:-docker-compose-prod.yml}
+
 docker-compose -f $DOCKER_CONFIG run --rm frontend gulp build-prod
-docker-compose -f $DOCKER_CONFIG run --rm django python manage.py collectstatic --noinput
+docker-compose -f $DOCKER_CONFIG_PROD run --rm djangoprod python3 manage.py collectstatic --noinput
