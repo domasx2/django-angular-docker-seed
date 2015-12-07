25c2f88cfc38c2de2fc042e38e7d2bca619fdb83
Domas <domasx2@gmail.com>
2015-12-07 21:04:21 +0200

    update python deps

--- backend/apps/sampleapp/urls.py
+++ backend/apps/sampleapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from . import views
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^tasks/$', views.TaskList.as_view(), name='task-list'),
     url(r'^tasks/(?P<slug>[a-zA-Z\-0-9]+)/?$', views.TaskDetail.as_view(), name='task-detail'),
-)
+]
--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.sampleapp.views import index as index_view
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + patterns('',
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'apps.sampleapp.views.index', name='index'),
+    url(r'^$', index_view, name='index'),
     url(r'^api/', include('apps.sampleapp.urls')),
-    url(r'^.*', 'apps.sampleapp.views.index') #catch-all
-)
+    url(r'^.*', index_view) #catch-all
+]
